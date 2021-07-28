1de390673be4dd6eca65e58a98b96a57f737772d
updonion <87442471+updonion@users.noreply.github.com>
2021-07-28 23:34:44 +0300

    Now it works really fast
    I want to make the telegram bot.

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,15 +1,19 @@
-import grequests
 from proxy_checker import ProxyChecker
+import multiprocessing.dummy as mp
 
 f = open("proxy.txt", 'r')
 good = open("proxy_valid.txt", 'a')
 lines = f.read().split('\n')
 checker = ProxyChecker()
-# print(lines)
-for i in lines:
-    prx = checker.check_proxy(i)
+
+def chp(i):
+    prx = checker.check_proxy(lines[i])
     if prx != False:
-        print(i, prx['anonymity'], "\nTimeout:", str(prx['timeout']), ("\nCountry:" + prx['country'] + "\n" if prx['country'] != '-' else '\n'))
+        print(lines[i], prx['anonymity'], "\nTimeout:", str(prx['timeout']), ("\nCountry:" + prx['country'] + "\n" if prx['country'] != '-' else '\n'))
 
-
+p=mp.Pool(100)
+p.map(chp,range(0,1000))
+p.close()
+p.join()
+    
 f.close()
