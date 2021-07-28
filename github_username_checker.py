75c0a0d35e40e790fb283f6d07719ef71e6d9cd9
updonion <87442471+updonion@users.noreply.github.com>
2021-07-28 16:14:33 +0300

    Check

--- github_username_checker.py
+++ github_username_checker.py
@@ -3,11 +3,15 @@ import requests
 alphabet = "pqrstuvwxyz"
 betbet = "0123456789abcdefghijklmnopqrstuvwxyz"
 f = open("free-github.txt", "a")
+dict = open("dict.txt", "r")
+words = dict.readlines()
 
-for a in alphabet:
-    for b in betbet:
-        page = requests.get('https://github.com/' + a + a + b + b)
-        if page.text == 'Not Found':
-            print(a + a + b + b)
-            f.write(a + a + b + b + "\n")
+# print(words)
+for word in words:
+    page = requests.get('https://github.com/' + word[:-1])
+    if page.text == 'Not Found':
+        
+        print(word[:-1])
+
+
 f.close()
