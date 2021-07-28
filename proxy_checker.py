2df12237b0f0767ece0c55d9fd2a5b17408379be
updonion <87442471+updonion@users.noreply.github.com>
2021-07-28 21:53:22 +0300

    Now I am using proxy_checker library
    Output example:
    
    196.15.221.200:80 Anonymous
    102.129.249.120:3128 Elite
    13.229.90.80:3128 Elite
    198.50.163.192:3129 Elite
    176.9.75.42:3128 Elite
    136.233.215.136:80 Anonymous
    198.199.86.11:8080 Elite
    101.132.186.39:9090 Elite
    196.15.221.205:80 Anonymous
    176.9.63.62:3128 Transparent
    176.193.32.111:8080 Transparent

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,24 +1,14 @@
-import requests
-import random
+import grequests
+from proxy_checker import ProxyChecker
 
-url = "http://api.my-ip.io/ip"
-
-proxies = {
-    "https": "106.110.218.39:4216"
-}
-
-f = open("proxy.txt", 'r') # your proxy list
+f = open("proxy.txt", 'r')
 good = open("proxy_valid.txt", 'a')
 lines = f.read().split('\n')
-print(lines)
+checker = ProxyChecker()
+# print(lines)
 for i in lines:
-    try:
-        proxy = {"https": i}
-        r = requests.get(url, proxies=proxy, timeout=1) # timeout 1 second
-        print(r.text)
-        good.write(i + "\n")
-    except:
-        print(i, "is bad")
-
+    prx = checker.check_proxy(i)
+    if prx != False:
+        print(i, prx['anonymity'])
 
 f.close()
